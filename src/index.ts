f9048ea7cdd384c36ab10b4fd7b1e77db7c6864e
ryanyogan <ryanyogan@gmail.com>
2020-05-03 10:25:02 -0700

    graphql is wired up

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { listings } from "./listings";
+import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
-
-app.use(express.json());
-
-app.get("/listings", (_req, res) => {
-  res.send(listings);
+const server = new ApolloServer({
+  schema,
 });
 
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send("failed to delete listing");
-});
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
