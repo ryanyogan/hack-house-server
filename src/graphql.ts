ba684c3bc600d37825c4f278a964c28639e559ac
ryanyogan <ryanyogan@gmail.com>
2020-05-03 10:45:38 -0700

    gql the hard-way... yup still the same under the hood

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,11 +1,38 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import {
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+import { listings } from "./listings";
+
+const Listing = new GraphQLObjectType({
+  name: "Listing",
+  fields: {
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLFloat) },
+  },
+});
 
 const query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "Hello from the Query!",
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => {
+        return listings;
+      },
     },
   },
 });
@@ -13,9 +40,20 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "Hello from the Mutation!",
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id === id) {
+            return listings.splice(i, 1)[0];
+          }
+        }
+
+        throw new Error("failed to delete listing");
+      },
     },
   },
 });
