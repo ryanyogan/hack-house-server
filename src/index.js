370d132ff842d3dc3279422e3706fc41beecfca4
ryanyogan <ryanyogan@gmail.com>
2020-05-03 08:49:33 -0700

    basic node server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = 9000;
+
+app.get("/", (_req, res) => res.send("hello world"));
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
