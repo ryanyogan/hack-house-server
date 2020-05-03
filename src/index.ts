c37ede9fa079fb6ec0500032a5b7bd168d2e6194
ryanyogan <ryanyogan@gmail.com>
2020-05-03 09:43:18 -0700

    two basic routes using the mock data to test everything works

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { listings } from "./listings";
 
 const app = express();
 const port = 9000;
 
-app.get("/", (_req, res) => res.send("hello world"));
+app.use(express.json());
+
+app.get("/listings", (_req, res) => {
+  res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send("failed to delete listing");
+});
 
 app.listen(port);
 
