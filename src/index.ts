570145290f400b9cf433752637634342760df9db
ryanyogan <ryanyogan@gmail.com>
2020-05-04 16:35:44 -0700

    now you saw the AST under-the-hood, here is the SDL way

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { schema } from "./graphql";
+import { resolvers, typeDefs } from "./graphql";
 
 const app = express();
 const port = 9000;
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
 });
 
 server.applyMiddleware({ app, path: "/api" });
-
 app.listen(port);
 
 console.log(`[app] : http://localhost:${port}`);
