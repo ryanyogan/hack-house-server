b6405268da48ffba826aa960462e4b271e76bc3b
ryanyogan <ryanyogan@gmail.com>
2020-05-13 09:20:06 -0700

    added new collections, typings for models, and new seed/truncate helpers

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Booking, Database, Listing, User } from "../lib/types";
 
 const user = process.env.DB_USER;
 const userPassword = process.env.DB_USER_PASSWORD;
@@ -15,6 +15,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,59 @@
 import { Collection, ObjectID } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
 export interface Listing {
   _id: ObjectID;
   title: string;
+  description: string;
   image: string;
+  host: string;
+  type: ListingType;
   address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectID[];
+  bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
+}
+
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletID?: string;
+  income: number;
+  bookings: ObjectID[];
+  listings: ObjectID[];
+}
+
+export interface Booking {
+  _id: ObjectID;
+  listing: ObjectID;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
